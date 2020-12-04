82b95661b676f1996c43160c415b407d27ecc0b4
George Pogosyan <george.pogosyan@asar.studio>
2020-12-04 13:59:40 +0300

    remove orthography matchers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { AggressiveTokenizer } from './tokenizers/aggressive_tokenizer';
-import { Matchers } from './tokenizers/orthography_matchers';
 import { RegexpTokenizer } from './tokenizers/regexp_tokenizer';
 import { WordTokenizer } from './tokenizers/word_tokenizer';
 import { WordPunctTokenizer } from './tokenizers/word_punct_tokenizer';
@@ -9,7 +8,6 @@ import { ngrams, bigrams, trigrams, multrigrams } from './ngrams/ngrams';
 
 export {
   AggressiveTokenizer,
-  Matchers,
   RegexpTokenizer,
   WordPunctTokenizer,
   PunctTokenizer,
